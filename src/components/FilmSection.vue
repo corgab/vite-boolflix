b98b096c83fc3b436f38240deb064ef8fa08fb92
corgab <corbanigabriele@gmail.com>
2024-04-16 18:11:19 +0200

    ADD: milestone 3 completata

--- src/components/FilmSection.vue
+++ src/components/FilmSection.vue
@@ -7,7 +7,7 @@
                 {{ film.original_name }}
                 <!-- {{ film.original_language }} -->
                 <span class="fi" :class="'fi-'+film.original_language"></span>
-                {{ film.vote_count }}
+                {{ convertVote(film.vote_average) }}
                 <img :src="`https://image.tmdb.org/t/p/${this.store.widthImg}${film.poster_path}`" alt="">
             </li>
         </ul>
@@ -20,6 +20,20 @@
         data() {
             return {
                 store,
+            }
+        },
+        methods: {
+            //da mettere globale ma non so farlo
+            convertVote(num) {
+                // converti il numero ad 1-5
+                const fiveVote = Math.round(num / 2)
+
+                //controllare se il numero è tra 1-5
+                if(fiveVote < 1) {
+                    return 1
+                } else if(fiveVote > 5) {
+                    return 5
+                } else return fiveVote
             }
         }
     }
