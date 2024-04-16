20863b746170ecd3cfc1f0eacb4f44a7e5981fc0
corgab <corbanigabriele@gmail.com>
2024-04-16 14:08:48 +0200

    ADD: Layout base

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <SearchBar/>
+</template>
+
+<script>
+  import SearchBar from "./components/SearchBar.vue";
+  export default {
+    components: {
+      SearchBar
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <input type="text" placeholder="Cerca il tuo Film">
+        <button>Cerca</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        // props: {
+        //     ob
+        // }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
